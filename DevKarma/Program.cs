ae7a8dd07bd07100bc081756f894f939ac23a03b
HeheheM <kefirowaty123@gmail.com>
2015-04-02 20:35:40 +0200

    Update Program.cs

--- DevKarma/Program.cs
+++ DevKarma/Program.cs
@@ -1,4 +1,4 @@
-﻿using DevCommom;
+using DevCommom;
 using LeagueSharp;
 using LeagueSharp.Common;
 using System;
@@ -88,7 +88,7 @@ namespace DevKarma
 
         private static void InitializeAttachEvents()
         {
-            Game.OnGameUpdate += Game_OnGameUpdate;
+            Game.OnUpdate += Game_OnGameUpdate;
             Drawing.OnDraw += Drawing_OnDraw;
             AntiGapcloser.OnEnemyGapcloser += AntiGapcloser_OnEnemyGapcloser;
             Interrupter.OnPossibleToInterrupt += Interrupter_OnPossibleToInterrupt;
